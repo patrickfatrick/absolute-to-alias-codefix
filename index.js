23265db812d8fbf8119b140b58880beb4f769771
Patrick Fricano <patrick.fricano@icloud.com>
2018-04-09 21:29:34 -0700

    prettier

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const chalk = require("chalk");
-const argv = require('minimist')(process.argv.slice(2), {
+const argv = require("minimist")(process.argv.slice(2), {
   alias: {
     location: "l",
     help: "h",
@@ -12,15 +12,10 @@ const argv = require('minimist')(process.argv.slice(2), {
   }
 });
 
-const {
-  help,
-  location = "src",
-  alias = "~",
-  directory = process.cwd()
-} = argv;
+const { help, location = "src", alias = "~", directory = process.cwd() } = argv;
 
-if (help)
-  return console.log(
+if (help) {
+  console.log(
     `
     absolute-to-alias-codefix [--location -l][--alias -a][--directory -d]
     
@@ -39,34 +34,65 @@ if (help)
     --alias -a alias to use in place of the absolute, defaults to "~"
     --directory -d absolute path to the root of the project, defaults to the current directory
     `
-  )
+  );
+  process.exit();
+}
 
-const topLevelFiles = fs.readdirSync(path.resolve(directory, location))
-  .filter(filename => !/node_modules/.test(filename) && filename.indexOf(".") !== 0)
-  .map(filename => filename.replace(/\.[a-z0-9]{1,5}$/i, "").replace(/\./g, "\\."));
-const importRegex = new RegExp(`^(import (?:.*)| *\\}) from (['"])(${topLevelFiles.join("|")})`, "gm");
-const requireRegex = new RegExp(`require\\((['"])(${topLevelFiles.join("|")})`, "g");
-const jestRegex = new RegExp(`jest\\.mock\\((['"])(${topLevelFiles.join("|")})`, "g");
+const topLevelFiles = fs
+  .readdirSync(path.resolve(directory, location))
+  .filter(
+    filename => !/node_modules/.test(filename) && filename.indexOf(".") !== 0
+  )
+  .map(filename =>
+    filename.replace(/\.[a-z0-9]{1,5}$/i, "").replace(/\./g, "\\.")
+  );
+
+const importRegex = new RegExp(
+  `^(import (?:.*)| *\\}) from (['"])(${topLevelFiles.join("|")})`,
+  "gm"
+);
+const requireRegex = new RegExp(
+  `require\\((['"])(${topLevelFiles.join("|")})`,
+  "g"
+);
+const jestRegex = new RegExp(
+  `jest\\.mock\\((['"])(${topLevelFiles.join("|")})`,
+  "g"
+);
+
 console.log("Converting all files in", chalk.blue(directory));
-console.log("Testing against", chalk.green("import"), chalk.yellow(importRegex));
-console.log("Testing against", chalk.green("require"), chalk.yellow(requireRegex));
+console.log(
+  "Testing against",
+  chalk.green("import"),
+  chalk.yellow(importRegex)
+);
+console.log(
+  "Testing against",
+  chalk.green("require"),
+  chalk.yellow(requireRegex)
+);
 console.log("Testing against", chalk.green("jest"), chalk.yellow(jestRegex));
 
 const convertAll = dir => {
   const contents = fs.readdirSync(dir);
   contents
-    .filter(filename => !/node_modules/.test(filename) && filename.indexOf(".") !== 0)
+    .filter(
+      filename => !/node_modules/.test(filename) && filename.indexOf(".") !== 0
+    )
     .forEach(filename => {
       const filepath = path.resolve(dir, filename);
       fs.readFile(filepath, "utf8", (err, data) => {
         if (err) return convertAll(path.resolve(dir, filename));
         if (!/\.jsx?$/.test(filepath)) return;
         const newData = data
-          .replace(importRegex, (_, p1, p2, p3) => `${p1} from ${p2}${alias}/${p3}`)
+          .replace(
+            importRegex,
+            (_, p1, p2, p3) => `${p1} from ${p2}${alias}/${p3}`
+          )
           .replace(requireRegex, (_, p1, p2) => `require(${p1}${alias}/${p2}`)
           .replace(jestRegex, (_, p1, p2) => `jest.mock(${p1}${alias}/${p2}`);
 
-        fs.writeFile(filepath, newData, "utf8", (err) => {
+        fs.writeFile(filepath, newData, "utf8", err => {
           if (err) return console.error(chalk.red(err));
           console.log(chalk.green("FIXED"), filepath);
         });
